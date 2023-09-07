ce9692f7d79e87c039eb68274f11a95fb22ee121
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-07 20:48:17 +0930

    Converted login to bootstrap
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- login.php
+++ login.php
@@ -11,9 +11,7 @@ if (DEBUG_MODE == true) {
     ini_set("display_errors", 1);
 }
 
-echo '<h2 class="w3-container w3-teal">Login</h2>';
-
-if (isset($_POST['username'])) {$CurrentUser = htmlentities(strip_tags($_POST['username']), ENT_SUBSTITUTE);}
+$tpl = new Template('templates/' . TEMPALTE);
 
 // Lets check to see if the call was a HTTP POST request
 //    If it is, display the admin page
@@ -22,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Now to check if the username field is not empty, otherwise throw an error.
     if (empty(trim($_POST["username"]))) {
-        echo "<div class='w3-panel w3-pale-red w3-display-container'>Username or password not supplied.</div>";
+        $login_error = true;
     } else {
         // We have data for a username, now lets save it in a SQL safe string (e.g. automatically add escape characters, etc.)
         $username = mysqli_real_escape_string($dbcon, $_POST['username']);
@@ -30,44 +28,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Do the same for the password field.
     if (empty(trim($_POST["password"]))) {
-        echo "<div class='w3-panel w3-pale-red w3-display-container'>Username or password not supplied!</div>";
+        $login_error = true;
     } else {
         // And again save the password in a SQL safe string
         $password = mysqli_real_escape_string($dbcon, $_POST['password']);
     }
 
-    // Build the SQL statement to get the user details (so we can then verify the user exists AND that the password is valid)
-    $sql = "SELECT `id`, `username`, `password`, `displayname` FROM users WHERE username = '$username'";
-
-    // Request the data from the SQL server, process it AND count the number of rows.
-    $result = mysqli_query($dbcon, $sql);
-    $row = mysqli_fetch_assoc($result);
-    $row_count = mysqli_num_rows($result);
-
-    // Check that the user only exists once in the SQL database AND that the password is matching.
-    if ($row_count == 1 && password_verify($password, $row['password'])) {
-        // This part we store some information in the PHP session information, so we can use it as a later time (e.g. the user ID)
-        $_SESSION['displayname'] = $row['displayname'];
-        $_SESSION['userid'] = $row['id'];
-        $_SESSION['username'] = $username;
-        $_SESSION["loggedin"] = true;
-
-        // Now we redirect the user to the admin portal.
-        header("location: admin.php");
+    if ($login_error) {
+        print $tpl->render('login-failed', array(
+            'url_path' => $url_path
+        ));
     } else {
-        echo "<div class='w3-panel w3-pale-red w3-display-container'>Incorrect username or password.</div>";
+
+        // Build the SQL statement to get the user details (so we can then verify the user exists AND that the password is valid)
+        $sql = "SELECT `id`, `username`, `password`, `displayname` FROM users WHERE username = '$username'";
+
+        // Request the data from the SQL server, process it AND count the number of rows.
+        $result = mysqli_query($dbcon, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $row_count = mysqli_num_rows($result);
+
+        // Check that the user only exists once in the SQL database AND that the password is matching.
+        if ($row_count == 1 && password_verify($password, $row['password'])) {
+            // This part we store some information in the PHP session information, so we can use it as a later time (e.g. the user ID)
+            $_SESSION['displayname'] = $row['displayname'];
+            $_SESSION['userid'] = $row['id'];
+            $_SESSION['username'] = $username;
+            $_SESSION["loggedin"] = true;
+
+            // Now we redirect the user to the admin portal.
+            header("location: admin.php");
+        } else {
+            print $tpl->render('login-failed', array(
+                'url_path' => $url_path
+            ));
+        }
     }
+} else {
+    print $tpl->render('login', array(
+        'url_path' => $url_path
+    ));
 }
-?>
-
-<form action="" method="POST" class="w3-container w3-padding">
-    <label>Username </label>
-    <input type="text" name="username" value="<?php if (isset($_POST['username'])) { echo $CurrentUser; } ?>" class="w3-input w3-border">
-    <label>Password</label>
-    <input type="password" name="password" class="w3-input w3-border">
-    <p><input type="submit" name="login" value="Login" class="w3-btn w3-teal"></p>
-</form>
-
-<?php
 
 include("footer.php");
---
+++ templates/default/login-failed.php
@@ -0,0 +1,47 @@
+<main class="container">
+    <div class="row g-5">
+        <div class="col-md-8">
+            <h3 class="pb-4 mb-4 fst-italic border-bottom">Login</h3>
+            <form class="needs-validation" novalidate method="POST">
+                <div class="col-sm-6 has-validation">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control is-invalid" id="username" name="username" placeholder="Enter your username here" required>
+                    <div class="invalid-feedback">
+                        A valid username is required.
+                    </div>
+                </div>
+                <div class="col-sm-6 has-validation">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" id="password" name="password" class="form-control is-invalid" required>
+                    <div class="invalid-feedback">
+                        A valid password is required.
+                    </div>
+                </div>
+                <div class="col-sm-6 justify-content-md-end">
+                    <button type="submit" class="btn btn-primary mb-3" name="login" value="Login">Login</button>
+                </div>
+            </form>
+            <script>
+                // Example starter JavaScript for disabling form submissions if there are invalid fields
+                (() => {
+                    'use strict'
+
+                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                    const forms = document.querySelectorAll('.needs-validation')
+
+                    // Loop over them and prevent submission
+                    Array.from(forms).forEach(form => {
+                        form.addEventListener('submit', event => {
+                            if (!form.checkValidity()) {
+                                event.preventDefault()
+                                event.stopPropagation()
+                            }
+
+                            form.classList.add('was-validated')
+                        }, false)
+                    })
+                })()
+            </script>
+        </div>
+    </div>
+</main>
