ba89e6ebd11810e323e202281407d946e9b03eb2
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-07 20:10:00 +0930

    Converted view post
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- templates/default/view_post.php
+++ templates/default/view_post.php
@@ -1,9 +1,11 @@
-<p>
-<article class="blog-post">
-    <h2 class="display-5 link-body-emphasis mb-1"><?php echo $title; ?></h2>
-    <p class="blog-post-meta"><?php echo $time; ?> by <?php echo $createdby; ?></p>
-
-    <p><?php echo $contents; ?></p>
-</article>
-</p>
-<hr />
+<main class="container">
+    <div class="row g-5">
+        <div class="col-md-8">
+            <article class="blog-post">
+                <h2 class="display-5 link-body-emphasis mb-1"><?php echo $title; ?></h2>
+                <p class="blog-post-meta"><?php echo $time; ?> by <?php echo $createdby; ?></p>
+                <p><?php echo $contents; ?></p>
+            </article>
+        </div>
+    </div>
+</main>
