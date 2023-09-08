cb614d0beb02c471f7b3eda2d4f003b69de40605
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-08 12:02:37 +0930

    Fixed layout (sizing & margins) for new posts
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- templates/default/new_post.php
+++ templates/default/new_post.php
@@ -3,20 +3,21 @@
         <div class="col-md-8">
             <h3 class="pb-4 mb-4 fst-italic border-bottom">New Post</h3>
             <form class="needs-validation" novalidate method="POST">
-                <div class="col-sm-6">
+                <div class="col-sm-8 mb-4">
                     <label for="firstName" class="form-label">Title</label>
                     <input type="text" class="form-control" id="Title" value="" name="title" required placeholder="Enter a catchy title">
                     <div class="invalid-feedback">
                         A valid title is required.
                     </div>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-auto mb-4">
                     <label for="firstName" class="form-label">Blog entry</label>
-                    <textarea class="form-control" id="description" row="30" cols="50" name="description" required placeholder="Enter something to write about"></textarea>
+                    <textarea class="form-control" id="description" style="height: 150px" name="description" required placeholder="Enter something to write about"></textarea>
                     <div class="invalid-feedback">
                         A valid blog entry is required.
                     </div>
                 </div>
+                <div></div>
                 <div class="col-sm-6">
                     <button type="submit" class="btn btn-primary mb-3" name="submit" value="Post">Create new blog post</button>
                 </div>
