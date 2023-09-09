ea5d849990b59d4eca72fa3b5ad6e39268c31e05
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-09 10:06:26 +0930

    Formatting.
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,21 +1,18 @@
-<footer class="py-5 text-center text-body-secondary bg-body-tertiary">
-    <p>Powered by Simple-PHP-Blog. Default template built by <a href="www.nephi.au">Nephi.AU</a>.</p>
-    <p class="mb-0">
-        <a href="#">Back to top</a>
-    </p>
-    <p>Copyright, all rights reserved | <?php echo date("Y"); ?></p>
-</footer>
-<!-- jQuery 3.7.1 -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<!-- Bootstrap v5.3.1 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo $url_path; ?>js/color-modes.js"></script>
-<!-- Bootstrap Table v1.22.1 -->
-<script src="<?php echo $url_path; ?>js/bootstrap-table.js"></script>
-<!-- <script src="<?php echo $url_path; ?>js/bootstrap-table-locale-all.js"></script> -->
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js"></script>
-
-</body>
-
+        <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+            <p>Powered by Simple-PHP-Blog. Default template built by <a href="www.nephi.au">Nephi.AU</a>.</p>
+            <p class="mb-0">
+                <a href="#">Back to top</a>
+            </p>
+            <p>Copyright, all rights reserved | <?php echo date("Y"); ?></p>
+        </footer>
+        <!-- jQuery 3.7.1 -->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <!-- Bootstrap v5.3.1 -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo $url_path; ?>js/color-modes.js"></script>
+        <!-- Bootstrap Table v1.22.1 -->
+        <script src="<?php echo $url_path; ?>js/bootstrap-table.js"></script>
+        <!-- <script src="<?php echo $url_path; ?>js/bootstrap-table-locale-all.js"></script> -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js"></script>
+    </body>
 </html>
