aacd7a4e791e91e9a1ecd3ea38433c5acbef7a98
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-10 21:43:25 +0930

    Minor formatting.
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- templates/default/login.php
+++ templates/default/login.php
@@ -10,12 +10,12 @@ if (isset($Login)) {
     echo '        <div class="col-md-8">' . "\r\n";
     echo '            <h3 class="pb-4 mb-4 fst-italic border-bottom">Login</h3>' . "\r\n";
     echo '            <form class="needs-validation" novalidate method="POST">' . "\r\n";
-    echo '                <div class="col-sm-6 has-validation">' . "\r\n";
+    echo '                <div class="col-sm-6 has-validation pb-1 mb-1">' . "\r\n";
     echo '                    <label for="username" class="form-label">Username</label>' . "\r\n";
     echo '                    <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username here" required>' . "\r\n";
     echo '                    <div class="invalid-feedback">A username is required.</div>' . "\r\n";
     echo '                </div>' . "\r\n";
-    echo '                <div class="col-sm-6 has-validation">' . "\r\n";
+    echo '                <div class="col-sm-6 has-validation pb-2 mb-2">' . "\r\n";
     echo '                    <label for="password" class="form-label">Password</label>' . "\r\n";
     echo '                    <input type="password" id="password" name="password" class="form-control" required>' . "\r\n";
     echo '                    <div class="invalid-feedback">A password is required.</div>' . "\r\n";
@@ -50,12 +50,12 @@ if (isset($LoginFailed)) {
     echo '        <div class="col-md-8">' . "\r\n";
     echo '            <h3 class="pb-4 mb-4 fst-italic border-bottom">Login</h3>' . "\r\n";
     echo '            <form class="needs-validation" novalidate method="POST">' . "\r\n";
-    echo '                <div class="col-sm-6 has-validation">' . "\r\n";
+    echo '                <div class="col-sm-6 has-validation pb-1 mb-1">' . "\r\n";
     echo '                    <label for="username" class="form-label">Username</label>' . "\r\n";
     echo '                    <input type="text" class="form-control is-invalid" id="username" name="username" placeholder="Enter your username here" required>' . "\r\n";
     echo '                    <div class="invalid-feedback">A valid username is required.</div>' . "\r\n";
     echo '                </div>' . "\r\n";
-    echo '                <div class="col-sm-6 has-validation">' . "\r\n";
+    echo '                <div class="col-sm-6 has-validation pb-2 mb-2">' . "\r\n";
     echo '                    <label for="password" class="form-label">Password</label>' . "\r\n";
     echo '                    <input type="password" id="password" name="password" class="form-control is-invalid" required>' . "\r\n";
     echo '                    <div class="invalid-feedback">A valid password is required.</div>' . "\r\n";
@@ -85,13 +85,12 @@ if (isset($LoginFailed)) {
 }
 
 if (isset($Login_Required)) {
-    echo '<meta http-equiv="refresh" content="2"; url="' . SITE_URL . 'login.php" />"' . "\r\n";
     echo '<main class="container">' . "\r\n";
     echo '    <div class="row g-5">' . "\r\n";
     echo '        <div class="col-md-8">' . "\r\n";
     echo '            <div class="alert alert-danger" role="alert">' . "\r\n";
     echo '                Error, Restricted page. Please login. Redirecting to login page in 2 seconds' . "\r\n";
-    echo '                <div class="spinner-border text-primary" role="status">' . "\r\n";
+    echo '                <div class="spinner-border text-secondary" role="status">' . "\r\n";
     echo '                    <span class="visually-hidden">Loading new login page...</span>' . "\r\n";
     echo '                </div>' . "\r\n";
     echo '            </div>' . "\r\n";
