487aac3267b9940ab2639ebf5ba581a64c4efdbf
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-10 19:53:27 +0930

    Created footer template.
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,18 +1,16 @@
-        <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
-            <p>Powered by Simple-PHP-Blog. Default template built by <a href="www.nephi.au">Nephi.AU</a>.</p>
-            <p class="mb-0">
-                <a href="#">Back to top</a>
-            </p>
-            <p>Copyright, all rights reserved | <?php echo date("Y"); ?></p>
-        </footer>
-        <!-- jQuery 3.7.1 -->
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <!-- Bootstrap v5.3.1 -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="<?php echo $url_path; ?>js/color-modes.js"></script>
-        <!-- Bootstrap Table v1.22.1 -->
-        <script src="<?php echo $url_path; ?>js/bootstrap-table.js"></script>
-        <!-- <script src="<?php echo $url_path; ?>js/bootstrap-table-locale-all.js"></script> -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js"></script>
-    </body>
-</html>
+<?php
+require_once 'includes.php';
+
+# Turn on debug mode, and show all errors.
+if (DEBUG_MODE == true) {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+}
+
+$tpl = new Template('templates/' . TEMPALTE);   // Creates the tpl object so we can reuse it
+
+// Display the top part of the admin page.
+print $tpl->render('footer', array(
+    'url_path' => $url_path,
+    'Footer' => true
+));
---
+++ templates/default/footer.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!isset($url_path)) {
+    die('invalid');
+}
+
+if (isset($Footer)) {
+    echo '        <footer class="py-5 text-center text-body-secondary bg-body-tertiary">' . "\r\n";
+    echo '            <p>Powered by Simple-PHP-Blog. Default template built by <a href="www.nephi.au">Nephi.AU</a>.</p>' . "\r\n";
+    echo '            <p class="mb-0">' . "\r\n";
+    echo '                <a href="#">Back to top</a>' . "\r\n";
+    echo '            </p>' . "\r\n";
+    echo '            <p>Copyright, all rights reserved | ' . date("Y") .'</p>' . "\r\n";
+    echo '        </footer>' . "\r\n";
+    echo '        <!-- jQuery 3.7.1 -->' . "\r\n";
+    echo '        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>' . "\r\n";
+    echo '        <!-- Bootstrap v5.3.1 -->' . "\r\n";
+    echo '        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>' . "\r\n";
+    echo '        <script src="<?php echo $url_path; ?>js/color-modes.js"></script>' . "\r\n";
+    echo '        <!-- Bootstrap Table v1.22.1 -->' . "\r\n";
+    echo '        <script src="<?php echo $url_path; ?>js/bootstrap-table.js"></script>' . "\r\n";
+    echo '        <!-- <script src="<?php echo $url_path; ?>js/bootstrap-table-locale-all.js"></script> -->' . "\r\n";
+    echo '        <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/trumbowyg.min.js"></script>' . "\r\n";
+    echo '    </body>' . "\r\n";
+    echo '</html>' . "\r\n";
+}
